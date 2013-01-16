89499e42daa84118d6afd868f11b29313817c86b
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-01-15 19:12:33 -0500

    updated release script

--- _bin/release.sh
+++ _bin/release.sh
@@ -1,8 +1,8 @@
-
+git checkout master && \
+rm -rf .awestruct/ .sass-cache/ _site/ && \
+git checkout develop && \
+git reset HEAD --hard && \
+awestruct && \
+awestruct --deploy -P production && \
+git reset HEAD --hard && \
 git checkout develop
-git reset HEAD --hard
-awestruct
-awestruct --deploy -P production
-git reset HEAD --hard
-git checkout develop
-
